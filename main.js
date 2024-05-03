40d16982a06273763202facb99ac1aeb01b6056a
mrdevanas <52702295+mrdevanas@users.noreply.github.com>
2024-05-03 22:19:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function p() {
+
+    document.getElementById("hi").innerHTML =
+    " <span style=color:red> iphone 14 </span> <br> <br>  <span style='color:black'>" + "CUPERTINO, CALIFORNIA Apple today introduced iPhone 14 and iPhone 14 Plus, featuring two sizes — 6.1 inches and 6.7 inches — in a sophisticated design with impressive camera upgrades and groundbreaking new safety capabilities. iPhone 14 and iPhone 14 Plus take stunning photos and video with a powerful camera system featuring new Main and front TrueDepth cameras, the Ultra Wide camera for unique perspectives, and Photonic Engine — an enhanced image pipeline. Both models include the A15 Bionic chip with a 5-core GPU, which offers incredible performance and efficiency for demanding workloads, and is designed with privacy and security built in. iPhone 14 and iPhone 14 Plus introduce critical safety capabilities such as Crash Detection and Emergency SOS via satellite, a first in the industry. And with amazing battery life, industry-leading durability features, and super-fast 5G, this iPhone lineup is more advanced than ever before. iPhone 14 and iPhone 14 Plus will be available in midnight, blue, starlight, purple, and (PRODUCT)RED1 finishes. Pre-orders begin Friday, September 9, with availability for iPhone 14 beginning Friday, September 16, and availability for iPhone 14 Plus beginning Friday, October 7."
+       + "</span>"
+    
+    
+    }
+    
+    
+    function r() {
+        document.getElementById("hi").innerHTML =
+    "<span style=color:black> iphone 14 </span>"
+    
+    }
+    
+    
+    function n() {
+        document.getElementById("sc").href = "https://pngimg.com/uploads/iphone_14/iphone_14_PNG9.png";
+    
+        document.getElementById("im").src = "https://pngimg.com/uploads/iphone_14/iphone_14_PNG9.png";
+    }
+    function b() {
+        document.getElementById("sc").href = "https://pngimg.com/uploads/iphone_14/iphone_14_PNG21.png";
+    
+        document.getElementById("im").src = "https://pngimg.com/uploads/iphone_14/iphone_14_PNG21.png";
+    }
